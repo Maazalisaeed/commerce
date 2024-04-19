5c864060676294dd60e0a26073b2b9d52147ae37
Maazalisaeed <maazali80009@gmail.com>
2024-04-20 00:12:26 +0500

    have added a dropdown menu in create new listing

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,12 +1,21 @@
 from django import forms
 from django.core.validators import MinValueValidator
 from .models import all_bids
+categories =( 
+    ("Fashion", "Fashion"), 
+    ("Electronics", "Electronics"), 
+    ("Home & Garden", "Home & Garden"), 
+    ("Collectibles & Hobbies", "Collectibles & Hobbies"), 
+    ("Sports & Outdoors", "Sports & Outdoors"),
+    ("Vehicles","Vehicles"),
+    ("Others","Others"), 
+)
 class new_listing_form(forms.Form): # this is a form that is created by django for data about a new listing on the commerce website
     title = forms.CharField(label="Title for the listing",max_length=80, required= True)
     description = forms.CharField(label="description", widget=forms.Textarea(attrs={'class':"description"}), required=True, max_length= 10000)
     image_url = forms.URLField(label="product image url (optional)", required=False)
     initial_bid = forms.FloatField(label="initial bid $",validators = [MinValueValidator(0)], required= True, widget=forms.NumberInput(attrs={'min': '0'}))
-    category = forms.CharField(label="plz enter a Category",max_length=255, required= True)
+    category = forms.ChoiceField(choices = categories,required=True)
 class biding_form(forms.Form):
     current_bid = forms.FloatField(label="Your bid $",validators = [MinValueValidator(0)], required= True, widget=forms.NumberInput(attrs={'min': '0'}))
     listing_id = forms.IntegerField(widget=forms.HiddenInput(), required=True)
